e2a4c760c4047534f81934fbdc8282516222dc75
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-06-19 15:17:36 -0300

    feat: Validado autenticaçao (SERVER)

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,92 +3,108 @@ import { error } from "console";
 import { parseCookies, setCookie } from "nookies";
 import { signOut } from "../context/AuthContext";
 
-let cookies = parseCookies();
 let isRefreshing = false;
 let failedRequestQueue = [];
 
-export const api = axios.create({
-  baseURL: "http://localhost:3333",
-  headers: {
-    Authorization: `Bearer ${cookies["nextauth.token"]}`,
-  },
-});
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
 
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error: AxiosError) => {
-    if (error.response.status === 401) {
-      {
-        if (error.response.data?.code === "token.expired") {
-          // renovar o token
-          cookies = parseCookies();
+  const api = axios.create({
+    baseURL: "http://localhost:3333",
+    headers: {
+      Authorization: `Bearer ${cookies["nextauth.token"]}`,
+    },
+  });
 
-          const { "nextauth.refreshToken": refreshToken } = cookies;
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error: AxiosError) => {
+      if (error.response.status === 401) {
+        {
+          if (error.response.data?.code === "token.expired") {
+            // renovar o token
+            cookies = parseCookies(ctx);
 
-          const originalConfig = error.config;
+            const { "nextauth.refreshToken": refreshToken } = cookies;
 
-          if (!isRefreshing) {
-            isRefreshing = true; // so faz refreshToken uma única vez
+            const originalConfig = error.config;
 
-            api
-              .post("/refresh", { refreshToken })
-              .then((response) => {
-                const { token } = response.data;
+            if (!isRefreshing) {
+              isRefreshing = true; // so faz refreshToken uma única vez
 
-                setCookie(undefined, "nextauth.token", token, {
-                  maxAge: 60 * 60 * 24 * 30,
-                });
+              // console.log("REFRESH")
 
-                setCookie(
-                  undefined,
-                  "nextauth.refreshToken",
-                  response.data.refreshToken,
-                  {
-                    maxAge: 60 * 60 * 24 * 30, // 30 dias
-                    path: "/",
+              api
+                .post("/refresh", { refreshToken })
+                .then((response) => {
+                  const { token } = response.data;
+
+                  setCookie(ctx, "nextauth.token", token, {
+                    maxAge: 60 * 60 * 24 * 30,
+                  });
+
+                  setCookie(
+                    ctx,
+                    "nextauth.refreshToken",
+                    response.data.refreshToken,
+                    {
+                      maxAge: 60 * 60 * 24 * 30, // 30 dias
+                      path: "/",
+                    }
+                  );
+
+                  api.defaults.headers["Authorization"] = ` Bearer ${token}`;
+
+                  failedRequestQueue.forEach((request) =>
+                    request.onSuccess(token)
+                  );
+
+                  failedRequestQueue = [];
+                })
+                .catch((err) => {
+                  failedRequestQueue.forEach((request) =>
+                    request.onFailure(err)
+                  );
+                  failedRequestQueue = [];
+
+                  if (process.browser) {
+                    signOut();
                   }
-                );
+                })
+                .finally(() => {
+                  isRefreshing = false;
+                });
+            }
 
-                api.defaults.headers["Authorization"] = ` Bearer ${token}`;
+            return new Promise((resolve, reject) => {
+              failedRequestQueue.push({
+                onSuccess: (token: string) => {
+                  originalConfig.headers["Authorization"] = `Bearer ${token}`;
 
-                failedRequestQueue.forEach((request) =>
-                  request.onSuccess(token)
-                );
+                  resolve(api(originalConfig));
+                },
 
-                failedRequestQueue = [];
-              })
-              .catch((err) => {
-                failedRequestQueue.forEach((request) => request.onFailure(err));
-              })
-              .finally(() => {
-                isRefreshing = false;
+                onFailure: (err: AxiosError) => {
+                  reject(err);
+                },
               });
+            });
+          } else {
+            // deslogar o usuário
+            if (process.browser) {
+              signOut();
+            }
           }
 
-          return new Promise((resolve, reject) => {
-            failedRequestQueue.push({
-              onSuccess: (token: string) => {
-                originalConfig.headers["Authorization"] = `Bearer ${token}`;
-
-                resolve(api(originalConfig));
-              },
-
-              onFailure: (err: AxiosError) => {
-                reject(err);
-              },
-            });
-          });
-        } else {
-          // deslogar o usuário
-          signOut();
+          return Promise.reject(error);
         }
-
-        return Promise.reject(error);
       }
     }
-  }
-);
+  );
+
+  return api;
+}
 
 // Quando o interceptors identificar que o token ta expirado, ele vai automaticamente pausar todas requisições que estão sendo feitas ao mesmo tempo e todas as requisições que vierem no futuro, até o token está realmente atualizado. Depois ele vai pegar aquelas requisições que não foram feitas porque o token não estava atualizado, vai executar elas novamente só que agora com o token atualizado. Vamos cria basicamente uma fila de requisições, essa fila vai armazenar todas as requisições que foram feitas para o back-end.
